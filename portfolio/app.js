582b88b6559774086e160d8d563c39f42d953db4
Masha <posicel@gmail.com>
2023-03-14 16:34:02 +0200

    gebug

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,22 +1,51 @@
-const slides = document.querySelectorAll("div.slide");
-console.log(slides);
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const slidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const slides = mainSlide.querySelectorAll('div')
+slidesCount = slides.length
+const container = document.querySelector('.container')
+const height = container.clientHeight
+console.log(height)
 
-for (const slide of slides) 
-    {
-        slide.addEventListener
-        (
-            'click', function()
-            { 
-                clearactiveClasses();
-                slide.classList.add('active');
-            }
-        )  
-    }; 
 
-function clearactiveClasses()
+let activeSlideIndex = 0
+let direction = ''
+
+slidebar.style.top = `-${(slidesCount-1) * 100}vh`
+
+upBtn.addEventListener('click', () => 
 {
-    for (const slide of slides) 
+    changeSlide('up')
+    console.log('up')
+})
+
+downBtn.addEventListener('click', () => 
+{
+    changeSlide('down')
+    console.log('down')
+})
+
+function changeSlide(direction)
+{
+    if (direction === 'up')
     {
-        slide.classList.remove('active');
-    }   
+        activeSlideIndex ++
+        if (activeSlideIndex === slidesCount)
+        {
+            activeSlideIndex = 0
+        }       
+    }
+    else if (direction === 'down')
+    {
+        activeSlideIndex --
+        if (activeSlideIndex < 0) 
+        {
+            activeSlideIndex = slidesCount - 1
+        }       
+    }
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+    
+    slidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
 }
